fab5db51b24c9e8f36be4a2c741948e3b0e853d4
veselovoleg <veselovoleg.nn@gmail.com>
2018-02-18 19:34:45 +0300

    Rename of some massives + added 3rd column aboutValues

--- src/main/java/printWeather.java
+++ src/main/java/printWeather.java
@@ -8,9 +8,11 @@ public class printWeather {
     public static void  cleanInfOfMeteoData () throws IOException {
 
         //Спиок наименований полученных значений
-        String[] columnNames = {"Температура ", "Ощущается, как ", "Атмосферное давление ",
+        String[] namesOfValues = {"Температура ", "Ощущается, как ", "Атмосферное давление ",
                 "Относительная влажность ", "Скорость ветра "};
-        String[] finalMeteoData = new String[5];
+        String[] aboutValues = {"°C","°C"," мм.рт.ст.","%"," м/сек." };
+
+        String[] valuesOfMeteoData = new String[5];
         //Вытягиваем значения температуры и тд
         Pattern pattern = Pattern.compile("[-+]?[0-9][0-9]?[0-9]?[.]?[0-9]?");
         Matcher matcher = pattern.matcher(workWithPageElements.getInfFromPagePublic().toString());
@@ -19,11 +21,11 @@ public class printWeather {
 
         while (matcher.find()) {
 
-            finalMeteoData[i] = matcher.group();
+            valuesOfMeteoData[i] = matcher.group();
             //Выводим значения в консоль
-            System.out.println(columnNames[i]+ finalMeteoData[i]); //Тут все выводится нормально - зачения температуры, владности и тд
+            System.out.println(namesOfValues[i]+ valuesOfMeteoData[i]+aboutValues[i]);
             i++;
         }
-      
+
     }
 }
