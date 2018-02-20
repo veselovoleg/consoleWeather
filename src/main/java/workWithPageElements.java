e87557750e80613e9861d5d02a613537ff43e35f
veselovoleg <veselovoleg.nn@gmail.com>
2018-02-20 21:48:41 +0300

    Added exceptions for workWithPageElements

--- src/main/java/workWithPageElements.java
+++ src/main/java/workWithPageElements.java
@@ -9,18 +9,27 @@ import java.net.URL;
 public class workWithPageElements {
     private static String url = "http://www.realmeteo.ru/moscow/1/current";
 
+
     //Метод, парсящий страницу с погодой
-    private  static Document getPage() throws IOException {
-        Document page = Jsoup.parse(new URL(url),3000);
+    private  static Document getPage() {
+        Document page = null;
+        try {
+            page = Jsoup.parse(new URL(url),3000);
+        } catch (IOException timeoutException) {
+            //timeoutException.printStackTrace();
+            System.out.println("В настоящее время сайт не доступен");
+        }
         return page;
+
     }
 
     //Достаем с сайта нужную нам информацию
-    private static Elements getInfFromPagePrivate () throws IOException {
+    private static Elements getInfFromPagePrivate ()  {
         Document page = getPage();
         Element getInformation = page.select("table[id=realdata]").first();
         Elements getMeteoData = getInformation.select("td[class=meteodata],td[class=meteodata_small]");
-        return getMeteoData;
+        if (page == null ||getMeteoData == null) System.out.println("Нет данных с сайта");
+                return getMeteoData;
     }
 
     //Паблик метод, который будем передавать в другой класс
